f2dbe410df139d6d32b65978dfa6ebb89389dd9c
Lopes, Dominique (EXTERN: MHP) <dominique.lopes@mhp.com>
2021-03-13 18:35:18 +0100

    fix terraform warnings
    azure ad group attribute deprecated: property `name` has been renamed to `display_name`
    aks clusters dns_prefix Interpolation-only expressions are deprecated: ${ ... } removed

--- terraform-manifests/07-aks-cluster.tf
+++ terraform-manifests/07-aks-cluster.tf
@@ -1,5 +1,5 @@
 resource "azurerm_kubernetes_cluster" "aks_cluster" {
-  dns_prefix          = "${azurerm_resource_group.aks_rg.name}"
+  dns_prefix          = azurerm_resource_group.aks_rg.name
   location            = azurerm_resource_group.aks_rg.location
   name                = "${azurerm_resource_group.aks_rg.name}-cluster"
   resource_group_name = azurerm_resource_group.aks_rg.name
