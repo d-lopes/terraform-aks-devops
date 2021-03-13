2e9dda989d2968844c8ada0ff979ca7237b47c27
Lopes, Dominique (EXTERN: MHP) <dominique.lopes@mhp.com>
2021-03-13 20:04:57 +0100

    reduce availability zones for node pools to stay within the allowed quota

--- terraform-manifests/07-aks-cluster.tf
+++ terraform-manifests/07-aks-cluster.tf
@@ -11,7 +11,7 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
     name       = "systempool"
     vm_size    = "Standard_DS2_v2"
     orchestrator_version = data.azurerm_kubernetes_service_versions.current.latest_version
-    availability_zones   = [1, 2, 3]
+    availability_zones   = [1, 2]
     enable_auto_scaling  = true
     max_count            = 3
     min_count            = 1
